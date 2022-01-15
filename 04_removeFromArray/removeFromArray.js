138d0a9c99ab5db65e0441aa43559d708ddae803
Dakoda Armstrong <mtlca66@gmail.com>
2022-01-15 14:21:46 -0700

    finished exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+  let array = args[0];
+  let newArray = [];    
 };
+
+removeFromArray(1,2,3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
