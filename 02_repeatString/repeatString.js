fc042fa4c01245aac576cc32cae1e1517eb5ac12
Dakoda Armstrong <mtlca66@gmail.com>
2022-01-15 11:45:16 -0700

    did first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) return 'ERROR';
+  let repeatedString = ''
+  while (num > 0) {
+      repeatedString += str;
+      num--;  
+  }
+  return repeatedString;
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
+
